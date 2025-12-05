720a627f7201e9890ee256dc7b1bb43af419f9f7
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-12-05 16:59:25 -0500

    ADD VSync boolean

--- Assets/Scripts/Framerate/FramerateManager.cs
+++ Assets/Scripts/Framerate/FramerateManager.cs
@@ -6,8 +6,10 @@ public class FramerateManager : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField,Range(30,120)] private int targetFramerate;
+    [SerializeField] private bool useVSync;
 
     private const int UNLIMITED_FPS_NUMBER = -1;
+    private const int VSYNC_ENABLED_NUMBER = 1;
 
     private void Awake()
     {
@@ -31,8 +33,14 @@ public class FramerateManager : MonoBehaviour
 
     private void SetTargetFramerate()
     {
+        //If VSync is on, targetFramerate will be ignored
+
         #if !UNITY_EDITOR
         Application.targetFrameRate = targetFramerate;
+        if (useVSync)
+        {
+            QualitySettings.vSyncCount = VSYNC_ENABLED_NUMBER;
+        }
         #else
         Application.targetFrameRate = UNLIMITED_FPS_NUMBER;
         #endif
